b532556e8c10ded94623f348ae501b9f2b95f5b3
RDG97 <ryangann1997@gmail.com>
2022-10-10 20:12:59 +0000

    modified:   .gitignore new file:   README.md new file:   package-lock.json new file:   package.json new file:   public/favicon.ico new file:   public/index.html new file:   public/logo192.png new file:   public/logo512.png new file:   public/manifest.json new file:   public/robots.txt new file:   src/App.css new file:   src/App.js new file:   src/App.test.js new file:   src/index.css new file:   src/index.js new file:   src/logo.svg new file:   src/reportWebVitals.js new file:   src/setupTests.js

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import logo from './logo.svg';
+import './App.css';
+import userEvent from '@testing-library/user-event';
+import { useState } from 'react';
+let x = 0;
+//function App() {
+//  return (
+//    <div className="App">
+//      <header className="App-header">
+//        <img src={logo} className="App-logo" alt="logo" />
+//        <p>
+//          Edit <code>src/App.js</code> and save to reload.
+//        </p>
+//        <a
+//          className="App-link"
+//          href="https://reactjs.org"
+//          target="_blank"
+//          rel="noopener noreferrer"
+//        >
+//          Learn React
+//        </a>
+//      </header>
+//    </div>
+//  );
+//}
+//export default App;
+
+function MyButton({ count, onClick }) {
+  return (
+    <button onClick={onClick}>
+      Clicked {count} times
+    </button>
+  );
+}
+
+function SquidButton() {
+  return (
+  <button onClick={Squid}>
+    press for squid
+  </button>
+  );
+}
+
+function ahh() {
+  console.log('test')
+}
+
+
+function Test() {
+  return (
+    <p>
+      im a test!
+    </p>
+    
+  );
+}
+
+function Squid() {
+  return (
+    <img
+      src="https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/61+3SAPKbcL._AC_SX679_.jpg"
+      alt="My man"
+      className="squid"
+
+    />
+  );
+}
+
+
+export default function MyApp() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
+      <Test />
+      <SquidButton />
+      <Squid />
+      <Squid />
+    </div>
+  );
+}
